d5983e63c11966d779e07fa6f723c2fabd417b34
Cem Bozsahin <cem.bozsahin@gmail.com>
2019-08-14 20:02:15 +0300

    globals defs now by defccglab

--- compile-tr.lisp
+++ compile-tr.lisp
@@ -3,22 +3,22 @@
 
 ;(in-package :auto-tr)
 
-(defparameter *VERBS-IN-GRAMMAR* NIL)
-(defparameter *lex-item-TEMPLATE* `((KEY nil) (PHON nil) (MORPH nil)
+(defccglab *VERBS-IN-GRAMMAR* NIL)
+(defccglab *lex-item-TEMPLATE* `((KEY nil) (PHON nil) (MORPH nil)
 		      (SYN nil)
 		      (SEM (LAM P (P X))) (PARAM 1.0)
 		      (TAG nil)))
-(defparameter *lex-rule-TEMPLATE* `((KEY nil) (INSYN NIL)(INSEM LF)
+(defccglab *lex-rule-TEMPLATE* `((KEY nil) (INSYN NIL)(INSEM LF)
 					(OUTSYN NIL)
 					(OUTSEM (LAM LF (LAM P (P LF))))
 					(INDEX NIL)
-					(PARAM 1.0)))
-(defparameter *SYNS* NIL)
-(defparameter *LAST-KEY-ID* NIL)
-(defparameter *ARGS* NIL)
-;(defparameter *MORPHS* '(TVING>)) ;now is given as an arg to main
-(defparameter *RAISED-LEX-RULES* NIL)
-(defparameter *RAISED-LEX-ITEMS* NIL)
+					(PARAM 1.0)))  ;; may be different if an .ind file is compiled --hhcb
+(defccglab *SYNS* NIL)
+(defccglab *LAST-KEY-ID* NIL)
+(defccglab *ARGS* NIL)
+;(defccglab *MORPHS* '(TVING>)) ;now is given as an arg to main
+(defccglab *RAISED-LEX-RULES* NIL)
+(defccglab *RAISED-LEX-ITEMS* NIL)
 ;--------get methods----------;
 
 (defun get-morph (v)
@@ -26,12 +26,6 @@
 
 (defun get-phon (v)
   (second (assoc 'PHON v)))
-
-(defun get-syn (l) ;only to print the SYN's of the entries of .ded file
-	"get syn of the grammar"
-	(dolist (keys l)
-		(dolist (catz keys)
-			(if (equal 'SYN (first catz)) (print catz)))))
 
 (defun get-modal-of-dir (l)
 	"if it is a complex cat, return the direction's modal if there is any"
@@ -161,17 +155,15 @@
   (load-gram arg)  ; ded and ind have same format--changed to load-gram
   (find-morph-v cl-user::*ccg-grammar* morphs)
   (get-last-key-id cl-user::*ccg-grammar*)
-  (dolist (keys *VERBS-IN-GRAMMAR*)
-    (dolist (cats-in-keys keys)
-      (if  (equal 'SYN (first cats-in-keys))   ; CB: first olmazsa SYN ne olacak?
-	(type-raise (second cats-in-keys))))
+  (dolist (v-entry *VERBS-IN-GRAMMAR*)
+    (type-raise (second (assoc 'SYN v-entry)))
     (loop while (not (equal 0 (length *SYNS*)))
-	  do (let ((temp (copy-alist *lex-rule-TEMPLATE*)))
-	       (set-insyn temp (pop *ARGS*))
-	       (set-outsyn temp (pop *SYNS*))
-	       (set-key temp (get-next-key-id))
-	       (set-index temp (gensym "auto-tr-"))
-	       (setf *RAISED-LEX-RULES* (append *RAISED-LEX-RULES* (wrap temp))))))
+	do (let ((temp (copy-alist *lex-rule-TEMPLATE*)))
+	     (set-insyn temp (pop *ARGS*))
+	     (set-outsyn temp (pop *SYNS*))
+	     (set-key temp (get-next-key-id))
+	     (set-index temp (gensym "auto-tr-"))
+	     (setf *RAISED-LEX-RULES* (append *RAISED-LEX-RULES* (wrap temp))))))
   (write-to-file "raised-lex-rules.ded" *RAISED-LEX-RULES*))
 
 ;it will throw an exception if not worked under cogs/core!!!
